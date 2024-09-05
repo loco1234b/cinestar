f64ea7ed7d1cc1b2fa49a29a1bff71af933df05d
aaron <aaronlurita123@gmail.com>
2024-09-05 13:45:57 -0500

    kk

--- fast_api.py
+++ fast_api.py
@@ -1,30 +1,29 @@
-
-import mysql.connector
 from fastapi import FastAPI
-
+import mysql.connector
 
 cnx = mysql.connector.connect(host='127.0.0.1', user='root', password='', database='cinestar')
 cursor = cnx.cursor(dictionary=True)
+
 app = FastAPI()
 
-#CORS(app)
 @app.get("/cines")
 def cines():
     cursor.callproc("sp_getCines")
     for row in cursor.stored_results() :
         cines = row.fetchall()
-        
-    cines={'success':True, 'data':cines, 'message':'lista de cines'}
 
+    #cines = {'success':True, 'data': cines, 'message': 'Lista de cines' }
     return cines
 
 @app.get("/cines/{id}")
-def cine(id:int):
+def cine(id: int):
     cursor.callproc("sp_getCine", [id])
     for row in cursor.stored_results() :
         cine = row.fetchone()
 
-    if cine is None : return dict([])
+    if cine is None : 
+        #return {'success':False, 'data': None, 'message': 'Cine no existe' }
+        return dict([])
 
     cursor.callproc("sp_getCinePeliculas", [id])
     for row in cursor.stored_results() :
@@ -36,12 +35,12 @@ def cine(id:int):
     
     cine['peliculas'] = peliculas
     cine['tarifas'] = tarifas
-    
-    #cine={'success':True, 'data':cine, 'message':'informacion de cine'}
+
+    #cine = {'success':True, 'data': cine, 'message': 'Informacón del cine' }
     return cine
 
 @app.get("/peliculas/{id}")
-def peliculas(id:str):
+def peliculas(id: str):
     id = 1 if id ==  'cartelera' else 2 if id == 'estrenos' else 0
     cursor.callproc("sp_getPeliculas", [id])
     for row in cursor.stored_results() :
@@ -51,8 +50,8 @@ def peliculas(id:str):
 
     return peliculas
 
-@app.get("/peliculas/<int:id>")
-def pelicula(id):
+@app.get("/pelicula/{id}")
+def pelicula(id: int):
     cursor.callproc("sp_getPelicula", [id])
     for row in cursor.stored_results() :
         pelicula = row.fetchone()
